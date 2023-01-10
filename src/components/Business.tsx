c9aae6168ae05d1f1440d3473e06ecf8596b9136
yefweb <yeferson.dev@gmail.com>
2023-01-09 22:09:18 -0500

    business section

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { features, feedback } from "../constans";
+import style, { layout } from "../style";
+import Button from "./Button";
+
+type props = { index: number };
+const FeacureCard = ({ ...feacture }, { index }: props) => {
+  const { icon, content, title } = feacture;
+  return (
+    <div className="flex flex-row p-6 rounded-[20px] gap-6 feature-card ">
+      <div
+        className={`w-[64px] h-[64px] rounded-full ${style.flexCenter} bg-dimBlue`}
+      >
+        <img
+          src={icon}
+          alt={title}
+          className="w-[50%] h-[50%] object-contain"
+        />
+      </div>
+
+      <div className="flex-1 flex flex-col ml-3 gap-2">
+        <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px]">
+          {title}
+        </h4>
+        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+          {content}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 const Business = () => {
-  return <div>Busnisess</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={style.heading2}>
+          You do the business, <br className="sm:block hidden" /> we&apos;ll
+          handle the money.
+        </h2>
+        <p className={`${style.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col gap-6`}>
+        {features.map((feacture, index) => {
+          return <FeacureCard index={index} key={feacture.id} {...feacture} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Business;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+type props = {
+  styles: string;
+};
+
+const Button = ({ styles }: props) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get started
+    </button>
+  );
 };
 
 export default Button;
