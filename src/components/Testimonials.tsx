0804fc656bb5bf4c298fedbc85e4826ee98dafb4
yefweb <yeferson.dev@gmail.com>
2023-01-10 14:02:27 -0500

    testimoniasls section

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { quotes } from "../assets";
+import { feedback } from "../constans";
+import styles from "../style";
 
-const Testimonials = () => {
-  return <div>Testimonials</div>;
+const FeedbackCard = ({ ...card }) => {
+  const { title, content, name, img } = card;
+
+  return (
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img
+        src={quotes}
+        alt="double_quotes"
+        className="w-[42px] h-[27px] object-contain"
+      />
+      <p className="font-poppins font-normal text-[18px] leading-[32px] text-white my-10 ">
+        {content}
+      </p>
+
+      <div className="flex flex-row ">
+        <img src={img} className="w-[48px] h-[48px] rounded-full" alt={title} />
+
+        <div className="flex flex-col ml-4">
+          <h3 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
+            {name}
+          </h3>
+          <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
+const Testimonials = () => (
+  <section
+    id="clients"
+    className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}
+  >
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2 className={styles.heading2}>
+        What People are <br className="sm:block hidden" /> saying about us
+      </h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+    </div>
+
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+      {feedback.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+);
 export default Testimonials;
